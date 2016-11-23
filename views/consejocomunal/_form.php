5b335496a5ea0ca9b5676f878adebc84ae31273f
leydyk93 <leydykm93@gmail.com>
2016-11-22 23:01:30 -0400

    CSSRegistrarCCleydy

--- views/consejocomunal/_form.php
+++ views/consejocomunal/_form.php
@@ -13,7 +13,17 @@ use app\models\Usuario;
 
 
 <div class="row">
-  <div class="col-sm-4">Opciones</div>
+  <div class="col-sm-4">
+  
+  <div class="list-group">
+  <a href="#" class="list-group-item text-center">Consejos Comunales</a>
+  <a href="#" class="list-group-item">Registrar Consejo Comunal</a>
+  <a href="#" class="list-group-item">Listar Consejo Comunal</a>
+  <a href="#" class="list-group-item text-center">Listar Consejo Comunal</a>
+  <a href="#" class="list-group-item">Contactar Vocero</a>
+</div>
+
+  </div>
 
   <div class="col-sm-8">
 
@@ -23,33 +33,61 @@ use app\models\Usuario;
     ]); ?>
 
     <div class="form-group">
-      <label for="">Táchira</label>
+      <label class="control-label col-sm-2"></label> 
+
     </div>
 
+    <div class="form-group">
 
-     <?= $form->field($municipios, 'idMunicipios')->dropDownList(
-            ArrayHelper::map(Municipios::find()->where(['Estados_idEstados'=>19])->all(),'idMunicipios','nombreMunicipios'),
-            ['prompt'=>'Seleccione Municipio',
+    <label class="control-label col-sm-2" >Municipio:</label>
+    <div class="col-sm-4">
+
+       <?= Html::activeDropDownList($municipios, 'idMunicipios',   ArrayHelper::map(Municipios::find()->where(['Estados_idEstados'=>19])->all(),'idMunicipios','nombreMunicipios'),
+          ['prompt'=>'Seleccione Municipio',
                 'onchange'=>'$.post("'.Yii::$app->getUrlManager()->getBaseUrl().'/consejocomunal/listarparroquias?id="+$(this).val(), function(data){
                  $( "select#consejocomunal-parroquias_idparroquias" ).html( data );      
             });',
+            'class' =>'form-control',
 
             ]
       ) ?>
 
-       <?= $form->field($model, 'Parroquias_idParroquias')->dropDownList(
-        ArrayHelper::map(Parroquias::find()->all(),'idParroquias','NombreParroquia'),
-        ['prompt'=>'Seleccione Parroquia']
-    ) ?>  
+     
+    </div>
+    <label class="control-label col-sm-2" for="email">Parroquia:</label>
+    <div class="col-sm-4">
+      <?= Html::activeDropDownList($model, 'Parroquias_idParroquias',ArrayHelper::map(Parroquias::find()->all(),'idParroquias','NombreParroquia'),
+         ['prompt'=>'Seleccione Parroquia',
+          'class' =>'form-control',
+      ]) ?>
+    </div>
+  </div>
 
-    <?= $form->field($model, 'NombreConsejoComunal')->textInput(['maxlength' => true]) ?>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="pwd">Nombre</label>
+    <div class="col-sm-10"> 
+      <?= Html::activeInput('text', $model, 'NombreConsejoComunal', ['class' => 'form-control']) ?>
+    </div>
+  </div>
 
-    <?= $form->field($model, 'fechaInscripcionConsejoComunal')->textInput()->label('Fecha ') ?>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="pwd">Fecha</label>
+    <div class="col-sm-10"> 
+      <?= Html::activeInput('text',$model, 'fechaInscripcionConsejoComunal', ['class' => 'form-control']) ?>
     
-    <?= $form->field($usuario, 'correoElectronico')->textInput() ?>
-
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Crear' : 'Modificar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+  </div>
+   
+   <div class="form-group">
+    <label class="control-label col-sm-2" for="pwd">Correo</label>
+    <div class="col-sm-10"> 
+      <?= Html::activeInput('text',$usuario, 'correoElectronico', ['class' => 'form-control']) ?>
+    
+    </div>
+  </div> 
+    
+    <div class="form-group text-center">
+        <?= Html::submitButton($model->isNewRecord ? 'Registrar' : 'Modificar', ['class' => $model->isNewRecord ? 'btn btn-info' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
--- views/consejocomunal/create.php
+++ views/consejocomunal/create.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
 /* @var $model app\models\Consejocomunal */
 
 $this->title = 'Create Consejocomunal';
-$this->params['breadcrumbs'][] = ['label' => 'Consejocomunals', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => 'Consejo Comunal', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="consejocomunal-create">

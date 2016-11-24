76b46abe68a1390a107a320b42b08f93b4cf391d
carlosrangel <crangel.unet@gmail.com>
2016-11-24 09:34:08 -0400

    Pagina home basica

--- views/site/index.php
+++ views/site/index.php
@@ -7,17 +7,63 @@ $this->title = 'My Yii Application';
 <div class="site-index" id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
 
     <div class="jumbotron text-center">
-      <h1>Company</h1>
-      <p>We specialize in blablabla</p>
+      <h1>Comunales</h1>
+      <p>El poder comunitario,la participación y el protaginismo popular, sus decisiones son, de carácter vinculante para el consejo comunal</p>
       <form class="form-inline">
-        <input type="email" class="form-control" size="50" placeholder="Email Address" required>
-        <button type="button" class="btn btn-danger">Subscribe</button>
+        <input type="email" class="form-control" size="50" placeholder="Buscar Consejo Comunal" required>
+        <a href="  <?php  echo Yii::$app->getUrlManager()->getBaseUrl(); ?>/consejocomunal/listarconsejo " type="button" class="btn btn-danger">Buscar</a>
       </form>
     </div>
 
 
+<!-- Container (Seccion 1 Principal) -->
+<div id="pricing" class="container-fluid">
+  <div class="row "> <!--slideanim">-->
+    <div class="col-sm-4 col-xs-12">
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h1>Verifica</h1>
+        </div>
+        <div class="panel-body">
+          <p><strong>Valida </strong> las cartas otorgadas por el Consejo Comunal</p>
+        </div>
+        <div class="panel-footer">
+          <button class="btn btn-lg">Verificar</button>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4 col-xs-12">
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h1>Encuentra</h1>
+        </div>
+        <div class="panel-body">
+          <p><strong> Los diferentes</strong>  Consejos Comunales</p>
+        </div>
+        <div class="panel-footer">
+          <input type="email" class="form-control" size="50" placeholder="Buscar Consejo Comunal" required>
+          <a href="  <?php  echo Yii::$app->getUrlManager()->getBaseUrl(); ?>/consejocomunal/listarconsejo " type="button" class="btn btn-danger">Buscar</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4 col-xs-12">
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h1>Conoce</h1>
+        </div>
+        <div class="panel-body">
+          <p><strong> Los pasos</strong>  para la cosntitución de un nuevo Consejos Comunales</p>
+        </div>
+        <div class="panel-footer">
+          <button class="btn btn-lg">Ver</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
-<!-- Container (About Section) -->
+
+<!-- Container (About Section) 
 <div id="about" class="container-fluid">
   <div class="row">
     <div class="col-sm-8">
@@ -31,260 +77,173 @@ $this->title = 'My Yii Application';
     </div>
   </div>
 </div>
-
+-->
 <div class="container-fluid bg-grey">
   <div class="row">
     <div class="col-sm-4">
       <span class="glyphicon glyphicon-globe logo slideanim"></span>
     </div>
     <div class="col-sm-8">
-      <h2>Our Values</h2><br>
-      <h4><strong>MISSION:</strong> Our mission lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4><br>
-      <p><strong>VISION:</strong> Our vision Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <h2>Funda Comunal</h2><br>
+      <h3><strong>¿Que es un Consejo Comunal?</strong></h3>
+      <h4> Es la forma de organización más avanzada que pueden darse los vecinos deuna determinada comunidadpara asumir el ejercicio real del poder popular .</h4><br>
     </div>
+    <div class="col-sm-12">
+      <h3>Pasos para conformar un nuevo Consejo Comunal</h3>
+      <div class="container-fluid text-center">
+        <div class="row "> <!--slideanim"> -->
+          <div class="col-sm-3">
+            <span class="glyphicon glyphicon-off logo-small"></span>
+            <p>Conformar equipo promotor</p>
+          </div>
+          <div class="col-sm-3">
+            <span class="glyphicon glyphicon-heart logo-small"></span>
+            <p>Difundir el alcance, objetivos y fines del Consejo Comunal</p>
+          </div>
+          <div class="col-sm-3">
+            <span class="glyphicon glyphicon-lock logo-small"></span>
+            <p>Elaborar el croquis del ámbito geográfico de la comunidad</p>
+          </div>
+          <div class="col-sm-3">
+            <span class="glyphicon glyphicon-leaf logo-small"></span>
+            <p>Elegir el equipo electoral provicional en asamblea ciudadana</p>
+          </div>
+          <div class="col-sm-3">
+            <span class="glyphicon glyphicon-certificate logo-small"></span>
+            <p>Elegir voceros y voceras en asamblea constitutiva</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-12">
+      <h3><strong>¿Eres coordinador Estadal?</strong></h3>
+      <h4>Registra los Consejos Comunales de tu jurisdicción</h4>
+      <a href="  <?php  echo Yii::$app->getUrlManager()->getBaseUrl(); ?>/consejocomunal/create " type="button" class="btn btn-">Registrar</a>
+      </div>
   </div>
 </div>
 
 <!-- Container (Services Section) -->
 <div id="services" class="container-fluid text-center">
-  <h2>SERVICES</h2>
-  <h4>What we offer</h4>
-  <br>
-  <div class="row slideanim">
+  <h2>Voceros</h2>
+  <h4>Que hacer si ya tienes el Consejo Comunal de tu comunidad constituido pero aun no estas registrado en el sistema</h4>
+  <div class="row "> <!--slideanim"> -->
     <div class="col-sm-4">
       <span class="glyphicon glyphicon-off logo-small"></span>
-      <h4>POWER</h4>
-      <p>Lorem ipsum dolor sit amet..</p>
+      <h4>Dirigite a la sede mas cercana de FundaComunal</h4>
     </div>
     <div class="col-sm-4">
       <span class="glyphicon glyphicon-heart logo-small"></span>
-      <h4>LOVE</h4>
-      <p>Lorem ipsum dolor sit amet..</p>
+      <h4>Consigna el acta constitutiva, correo electrónico del Consejo Comunal</h4>      
     </div>
     <div class="col-sm-4">
       <span class="glyphicon glyphicon-lock logo-small"></span>
-      <h4>JOB DONE</h4>
-      <p>Lorem ipsum dolor sit amet..</p>
+      <h4>Ingresa al correo electronico suministrado y verifica la cuenta</h4>      
     </div>
   </div>
-  <br><br>
-  <div class="row slideanim">
-    <div class="col-sm-4">
-      <span class="glyphicon glyphicon-leaf logo-small"></span>
-      <h4>GREEN</h4>
-      <p>Lorem ipsum dolor sit amet..</p>
+  <div class="row "> <!--slideanim">-->
+    <div class="col-sm-6 col-xs-12">
+      <div class="panel panel-default text-center">    
+        <div class="panel-body">
+          <p><strong>Consulta </strong> por diversos criterios los miembros de tu Consejo Comunal</p>
+        </div>
+        <div class="panel-heading">
+          <h1>Consulta</h1>
+        </div>
+      </div>
     </div>
-    <div class="col-sm-4">
-      <span class="glyphicon glyphicon-certificate logo-small"></span>
-      <h4>CERTIFIED</h4>
-      <p>Lorem ipsum dolor sit amet..</p>
-    </div>
-    <div class="col-sm-4">
-      <span class="glyphicon glyphicon-wrench logo-small"></span>
-      <h4 style="color:#303030;">HARD WORK</h4>
-      <p>Lorem ipsum dolor sit amet..</p>
+    <div class="col-sm-6 col-xs-12">
+      <div class="panel panel-default text-center">    
+        <div class="panel-body">
+          <p><strong>Genera </strong> el cuadero electoral y otros reportes de tu Consejo Comunal</p>
+        </div>
+        <div class="panel-heading">
+          <h1>Genera</h1>
+        </div>
+      </div>
     </div>
   </div>
 </div>
 
 <!-- Container (Portfolio Section) -->
 <div id="portfolio" class="container-fluid text-center bg-grey">
-  <h2>Portfolio</h2><br>
-  <h4>What we have created</h4>
-  <div class="row text-center slideanim">
+  <h2>Familias</h2>
+  <div class="row text-center "> <!--slideanim"> -->
     <div class="col-sm-4">
       <div class="thumbnail">
-        <img src="paris.jpg" alt="Paris" width="400" height="300">
-        <p><strong>Paris</strong></p>
-        <p>Yes, we built Paris</p>
+        <p><strong>Busca tu Consejo Cocumal y registra a os miembros de tu familia</strong></p>
+        <input type="email" class="form-control" size="50" placeholder="Buscar Consejo Comunal" required>
+        <button class="btn btn-lg">Buscar</button>
+        <br><br>
+
+        <p>No conoce el nombre del Consejo Cocumal, realiza una busqueda por Estado, Municio o parroquia</p>
+        <button class="btn btn-lg">Buscar</button>
+
       </div>
     </div>
     <div class="col-sm-4">
-      <div class="thumbnail">
-        <img src="newyork.jpg" alt="New York" width="400" height="300">
-        <p><strong>New York</strong></p>
-        <p>We built New York</p>
+      <div class="thumbnail">        
+        <p><strong>Verifica</strong></p>
+        <img src="newyork.jpg" alt="Familia" width="400" height="300">
+        <p>Si tu familia ya se encuentra registrada en el Consejo Comunal de tu comunidad</p>
+        <button class="btn btn-lg">Verficar familia</button>
       </div>
     </div>
     <div class="col-sm-4">
-      <div class="thumbnail">
-        <img src="sanfran.jpg" alt="San Francisco" width="400" height="300">
-        <p><strong>San Francisco</strong></p>
-        <p>Yes, San Fran is ours</p>
+      <div class="thumbnail">        
+        <p><strong>Registrate</strong></p>
+        <img src="sanfran.jpg" alt="Registrar" width="400" height="300">
+        <p>Tu familia aún no se encuentra registrada en el Consejo Comunal de tu comunidad? o no posees cuenta para ingresar al sistema?</p>
+        <button class="btn btn-lg">Registrar familia</button>
+
       </div>
     </div>
   </div><br>
-  
-  <h2>What our customers say</h2>
-  <div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-      <div class="item active">
-        <h4>"This company is the best. I am so happy with the result!"<br><span style="font-style:normal;">Michael Roe, Vice President, Comment Box</span></h4>
-      </div>
-      <div class="item">
-        <h4>"One word... WOW!!"<br><span style="font-style:normal;">John Doe, Salesman, Rep Inc</span></h4>
-      </div>
-      <div class="item">
-        <h4>"Could I... BE any more happy with this company?"<br><span style="font-style:normal;">Chandler Bing, Actor, FriendsAlot</span></h4>
-      </div>
-    </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div>
-
-<!-- Container (Pricing Section) -->
-<div id="pricing" class="container-fluid">
-  <div class="text-center">
-    <h2>Pricing</h2>
-    <h4>Choose a payment plan that works for you</h4>
-  </div>
-  <div class="row slideanim">
-    <div class="col-sm-4 col-xs-12">
-      <div class="panel panel-default text-center">
-        <div class="panel-heading">
-          <h1>Basic</h1>
-        </div>
-        <div class="panel-body">
-          <p><strong>20</strong> Lorem</p>
-          <p><strong>15</strong> Ipsum</p>
-          <p><strong>5</strong> Dolor</p>
-          <p><strong>2</strong> Sit</p>
-          <p><strong>Endless</strong> Amet</p>
-        </div>
-        <div class="panel-footer">
-          <h3>$19</h3>
-          <h4>per month</h4>
-          <button class="btn btn-lg">Sign Up</button>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm-4 col-xs-12">
-      <div class="panel panel-default text-center">
-        <div class="panel-heading">
-          <h1>Pro</h1>
-        </div>
-        <div class="panel-body">
-          <p><strong>50</strong> Lorem</p>
-          <p><strong>25</strong> Ipsum</p>
-          <p><strong>10</strong> Dolor</p>
-          <p><strong>5</strong> Sit</p>
-          <p><strong>Endless</strong> Amet</p>
-        </div>
-        <div class="panel-footer">
-          <h3>$29</h3>
-          <h4>per month</h4>
-          <button class="btn btn-lg">Sign Up</button>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm-4 col-xs-12">
-      <div class="panel panel-default text-center">
-        <div class="panel-heading">
-          <h1>Premium</h1>
-        </div>
-        <div class="panel-body">
-          <p><strong>100</strong> Lorem</p>
-          <p><strong>50</strong> Ipsum</p>
-          <p><strong>25</strong> Dolor</p>
-          <p><strong>10</strong> Sit</p>
-          <p><strong>Endless</strong> Amet</p>
-        </div>
-        <div class="panel-footer">
-          <h3>$49</h3>
-          <h4>per month</h4>
-          <button class="btn btn-lg">Sign Up</button>
-        </div>
-      </div>
-    </div>
-  </div>
 </div>
 
 <!-- Container (Contact Section) -->
-<div id="contact" class="container-fluid bg-grey">
-  <h2 class="text-center">CONTACT</h2>
+<div id="contact" class="container-fluid ">
+  <h2 class="text-center">CONTACTO</h2>
   <div class="row">
     <div class="col-sm-5">
-      <p>Contact us and we'll get back to you within 24 hours.</p>
-      <p><span class="glyphicon glyphicon-map-marker"></span> Chicago, US</p>
-      <p><span class="glyphicon glyphicon-phone"></span> +00 1515151515</p>
-      <p><span class="glyphicon glyphicon-envelope"></span> myemail@something.com</p>
+      <p>Póngase en contacto con nosotros y nos pondremos en contacto con usted en 24 horas..</p>
+      <p><span class="glyphicon glyphicon-map-marker"></span> Caracas, VE</p>
+      <p><span class="glyphicon glyphicon-phone"></span> +58 21212345678</p>
+      <p><span class="glyphicon glyphicon-envelope"></span> contacto@comunal.com</p>
     </div>
-    <div class="col-sm-7 slideanim">
+    <div class="col-sm-7 "> <!-- slideanim">  -->
       <div class="row">
         <div class="col-sm-6 form-group">
-          <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+          <input class="form-control" id="name" name="name" placeholder="Nombre" type="text" required>
+        </div>
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="name" name="name" placeholder="Apellido" type="text" required>
+        </div>
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="state" name="state" placeholder="Estado" type="text" required>
         </div>
         <div class="col-sm-6 form-group">
           <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
         </div>
+        <div class="col-sm-12 form-group">
+          <input class="form-control" id="asunto" name="asunto" placeholder="Asunto" type="text" required>
+        </div>
       </div>
-      <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea><br>
+      <textarea class="form-control" id="comments" name="comments" placeholder="Comentario" rows="5"></textarea><br>
       <div class="row">
         <div class="col-sm-12 form-group">
-          <button class="btn btn-default pull-right" type="submit">Send</button>
+          <button class="btn btn-default pull-right" type="submit">Enviar</button>
         </div>
       </div>
     </div>
   </div>
 </div>
 
-<div id="googleMap" style="height:400px;width:100%;"></div>
-
-
-
-
-
-
-
-<!-- Add Google Maps -->
-<script src="https://maps.googleapis.com/maps/api/js"></script>
-<script>
-var myCenter = new google.maps.LatLng(41.878114, -87.629798);
-
-function initialize() {
-var mapProp = {
-  center:myCenter,
-  zoom:12,
-  scrollwheel:false,
-  draggable:false,
-  mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-
-var marker = new google.maps.Marker({
-  position:myCenter,
-  });
-
-marker.setMap(map);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-</script>
 
 <footer class="container-fluid text-center">
   <a href="#myPage" title="To Top">
     <span class="glyphicon glyphicon-chevron-up"></span>
   </a>
-  <p>Bootstrap Theme Made By <a href="http://www.w3schools.com" title="Visit w3schools">www.w3schools.com</a></p>
 </footer>
 
 <script>
